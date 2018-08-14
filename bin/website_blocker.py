69ca225b6db5fa8e67acddbdc2176e9d211c7cc9
Dimitar Nonov <d.nonov@gmail.com>
2018-08-14 17:09:42 +0100

    bin: Add python script
    Script that blocks all blacklisted websites during the work hours.
    Websites like youtube, facebook, twitter etc.

---
+++ bin/website_blocker.py
@@ -0,0 +1,34 @@
+import time
+from datetime import datetime as dt
+
+host_path    = "/etc/hosts"
+redirect     = "127.0.0.1"
+black_list = ["www.facebook.com", "www.youtube.com", "www.twiter.com"]
+
+def hour_bound(hour):
+    return dt(dt.now().year, dt.now().month, dt.now().day, hour)
+
+current_time = dt.now()
+finish_hour  = hour_bound(20)
+start_hour   = hour_bound(8)
+
+while True:
+    if start_hour < current_time < finish_hour:
+        with open(host_path, "r+") as file:
+            content = file.read()
+            for website in black_list:
+                if website in content:
+                    pass
+                else:
+                    file.write(redirect + " " + website + "\n")
+        print(content)
+    else:
+        with open(host_path, "r+") as file:
+            content = file.readlines()
+            file.seek(0)
+            for line in content:
+                if not any(website in line for website in black_list):
+                    file.write(line)
+            file.truncate()
+        print(content)
+    time.sleep(5)
