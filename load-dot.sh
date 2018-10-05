a2cd010c3b9a6d02a50efeade7bf9ef8d5fc0118
Dimitar Nonov <d.nonov@gmail.com>
2018-10-05 15:59:56 +0100

    [feat][style] Add git commit message template
    This will increase the amount of ingormation in each git commit message.
    It will give more structured look to the message.

--- load-dot.sh
+++ load-dot.sh
@@ -3,6 +3,7 @@
 cp -p .bashrc /home/dim;
 cp -p .vimrc /home/dim;
 cp -p .gitconfig /home/dim;
+cp -p .gitmessage /home/dim;
 cp -p .tmux.conf /home/dim;
 cp -p .zshrc /home/dim;
 cp -Rp bin /home/dim;
