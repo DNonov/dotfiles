c9375127eddeb6eb6caa5aed1d916cb3931045dd
Dimitar Nonov <d.nonov@gmail.com>
2018-10-15 23:34:13 +0100

    [refactor] Use all files in startup and fix load
    Add all files from ".vim/startup" folder on the runtime in the vimrc
    file. Fix the load-dot.sh script to copy/past the updated version of
    dotfiles.

--- load-dot.sh
+++ load-dot.sh
@@ -1,9 +1,12 @@
 #! /bin/bash
 
 cp -p .bashrc /home/dim;
-cp -p .vimrc /home/dim;
 cp -p .gitconfig /home/dim;
 cp -p .gitmessage /home/dim;
 cp -p .tmux.conf /home/dim;
 cp -p .zshrc /home/dim;
 cp -Rp bin /home/dim;
+
+# VIM
+cp -p .vimrc /home/dim/.vim/vimrc;
+cp -Rp startup /home/dim/.vim/;
