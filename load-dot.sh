769fe35965d3eac706e1e7f93eb75ac31741dfe6
Dimitar Nonov <d.nonov@gmail.com>
2018-10-23 10:22:55 +0100

    [refactor] Rename all dot files and remove bashrc
    Files now can be seen, and will be just pasted as hidden at "~/".
    I don't use bash anymore, so I don't need .bashrc.

--- load-dot.sh
+++ load-dot.sh
@@ -1,14 +1,16 @@
 #! /bin/bash
 
-cp -p .bashrc /home/dim;
-cp -p .gitconfig /home/dim;
-cp -p .gitmessage /home/dim;
-cp -p .tmux.conf /home/dim;
-cp -p .zshrc /home/dim;
-cp -p .mongorc.js /home/dim;
-cp -Rp bin /home/dim;
+# DOTS
+cp gitconfig  /home/dim/.gitconfig;
+cp gitmessage /home/dim/.gitmessage;
+cp tmux.conf  /home/dim/.tmux;
+cp zshrc      /home/dim/.zshrc/.zshrc;
+cp mongorc.js /home/dim/.mongorc;
+
+# BIN
+cp -R bin /home/dim;
 
 # VIM
-cp -p vimrc /home/dim/.vim/vimrc;
-cp -Rp ./vim/startup /home/dim/.vim/;
-cp -Rp ./vim/colors /home/dim/.vim/;
+cp    vimrc         /home/dim/.vim/vimrc;
+cp -R ./vim/startup /home/dim/.vim/;
+cp -R ./vim/colors  /home/dim/.vim/;
