99e3e33adfedfceae217fca8aa89a79a49366ed3
Dimitar Nonov <d.nonov@gmail.com>
2018-10-31 17:55:08 +0000

    [refactor] Rename and improve the setup script

--- setup.sh
+++ setup.sh
@@ -4,8 +4,11 @@
 cp gitconfig  /home/dim/.gitconfig;
 cp gitignore  /home/dim/.gitignore;
 cp gitmessage /home/dim/.gitmessage;
+
+# TMUX
 cp tmux.conf  /home/dim/.tmux.conf;
-cp zshrc      /home/dim/.zshrc;
+
+# MONGO
 cp mongorc.js /home/dim/.mongorc.js;
 
 # BIN
@@ -15,3 +18,9 @@ cp -R bin /home/dim;
 cp    vimrc         /home/dim/.vim/vimrc;
 cp -R ./vim/startup /home/dim/.vim/;
 cp -R ./vim/colors  /home/dim/.vim/;
+
+#ZSH
+cp ./zsh/aliases   /home/dim/.zsh;
+cp ./zsh/config    /home/dim/.zsh;
+cp ./zsh/functions /home/dim/.zsh;
+cp zshrc           /home/dim/.zshrc;
