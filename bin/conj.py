5f2fdcba04cca004da6b71f679a5eba0f16016bc
Dimitar Nonov <d.nonov@gmail.com>
2019-01-11 14:38:12 +0000

    [feat] Add missing argument check

--- bin/conj.py
+++ bin/conj.py
@@ -4,21 +4,24 @@ from urllib.request import urlopen
 import sys
 from bs4 import BeautifulSoup
 
-verb_to_check = sys.argv[1] + ".html"
+if len(sys.argv) > 1:
+    verb_to_check = sys.argv[1] + ".html"
 
-URL = "http://conjugator.reverso.net/conjugation-english-verb-"
-URL += verb_to_check
+    URL = "http://conjugator.reverso.net/conjugation-english-verb-"
+    URL += verb_to_check
 
-page = urlopen(URL)
-soup = BeautifulSoup(page, "html.parser")
+    page = urlopen(URL)
+    soup = BeautifulSoup(page, "html.parser")
 
-pronouns = soup.find_all("i", attrs={"class": "graytxt"})[0:12]
-verbs = soup.find_all("i", attrs={"class": "verbtxt"})[0:12]
+    pronouns = soup.find_all("i", attrs={"class": "graytxt"})[0:12]
+    verbs = soup.find_all("i", attrs={"class": "verbtxt"})[0:12]
 
-if verb_to_check in verbs:
-    for i in range(12):
-        stripped_pronoun = pronouns[i].text.strip()
-        stripped_verb = verbs[i].text.strip()
-        print(stripped_pronoun + " " + stripped_verb)
+    if len(verbs) != 0:
+        for i in range(12):
+            stripped_pronoun = pronouns[i].text.strip()
+            stripped_verb = verbs[i].text.strip()
+            print(stripped_pronoun + " " + stripped_verb)
+    else:
+        print("There is no such a verb.")
 else:
-    print("There is no such a verb.")
+    print("Missing argument.")
