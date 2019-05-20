97c2d6109ad53dfda5d33531e8dda9e2f09523ac
Dimitar Nonov <d.nonov@gmail.com>
2019-05-20 21:11:35 +0100

    [feat] Update setup.sh file

--- setup.sh
+++ setup.sh
@@ -1,36 +1,92 @@
 #! /bin/bash
 
+# Add colors util
+. ./utils/colors.sh
+
+echo -e "${Orange}Copying of files has been started!"
+
 # GIT
-cp gitconfig  ~/.gitconfig;
-cp gitignore  ~/.gitignore;
-cp gitmessage ~/.gitmessage;
+rm ~/.gitconfig;
+rm ~/.gitignore;
+rm ~/.gitmessage;
+cp --preserve=all gitconfig  ~/.gitconfig;
+echo -e "${Green}--> Copy" "${Blue}.gitconfig" "${Green}successfully!"
+cp --preserve=all gitignore  ~/.gitignore;
+echo -e "${Green}--> Copy" "${Blue}.gitignore" "${Green}successfully!"
+cp --preserve=all gitmessage ~/.gitmessage;
+echo -e "${Green}--> Copy" "${Blue}.gitmessage" "${Green}successfully!"
 
 # TMUX
-cp tmux.conf ~/.tmux.conf;
+rm ~/.tmux.conf;
+cp --preserve=all tmux.conf ~/.tmux.conf;
+echo -e "${Green}--> Copy" "${Blue}.tmux.conf" "${Green}successfully!"
 
 # MONGO
-cp mongorc.js ~/.mongorc.js;
-
-# BIN
-cp -r bin ~/;
-
-# VIM
-cp    vimrc         ~/.vim/vimrc;
-cp -r ./vim/startup ~/.vim/;
-cp -r ./vim/after   ~/.vim/;
-cp -r ./vim/colors  ~/.vim/;
-
-# ZSH
-cp     zshrc ~/.zshrc;
-rm -rf ~/.zsh;
-cp -r  ./zsh ~/.zsh;
+rm ~/.mongorc.js;
+cp --preserve=all mongorc.js ~/.mongorc.js;
+echo -e "${Green}--> Copy" "${Blue}.mongorc.js" "${Green}successfully!"
 
 # TERN
+rm ~/.tern-config;
 cp ./tern-config ~/.tern-config;
+echo -e "${Green}--> Copy" "${Blue}.tern-config" "${Green}successfully!"
 
 # ESLint
+rm ~/.eslintrc.json
 cp ./eslintrc.json ~/.eslintrc.json;
+echo -e "${Green}--> Copy" "${Blue}.eslintrc.json" "${Green}successfully!"
+
+# ZSH
+rm ~/.zshrc;
+cp zshrc ~/.zshrc;
+echo -e "${Green}--> Copy" "${Blue}.zshrc" "${Green}successfully!"
+
+# VIM
+rm ~/.vim/vimrc
+cp --preserve=all vimrc ~/.vim/vimrc;
+echo -e "${Green}--> Copy" "${Blue}vimrc" "${Green}successfully!"
+
+echo -e "${Orange}Copying of files has been finished!"
+
+#===============================================================================
+
+echo -e "${Orange}Copying of folders has been started!"
+
+# BIN
+rm -rf ~/bin;
+cp --preserve=all -r bin ~/bin;
+echo -e "${Green}--> Copy" "${Blue}bin" "${Green}successfully!"
+
+
+# VIM
+rm -rf ~/.vim/startup
+rm -rf ~/.vim/after
+rm -rf ~/.vim/colors
+cp --preserve=all -r ./vim/startup ~/.vim/startup;
+echo -e "${Green}--> Copy" "${Blue}.vim/startup" "${Green}successfully!"
+cp --preserve=all -r ./vim/after   ~/.vim/after;
+echo -e "${Green}--> Copy" "${Blue}.vim/after" "${Green}successfully!"
+cp --preserve=all -r ./vim/colors  ~/.vim/colors;
+echo -e "${Green}--> Copy" "${Blue}.vim/colors" "${Green}successfully!"
+
+# ZSH
+rm -rf ~/.zsh;
+cp -r  ./zsh ~/.zsh;
+echo -e "${Green}--> Copy" "${Blue}.zsh" "${Green}successfully!"
+echo -e "${Orange}Copying of folders has been finished!"
+
+#===============================================================================
 
 # VS Code
-cp ./code/settings.json ~/.config/Code/User/settings.json
+
+# Import the linker
+. ./code/settings.sh
+
+# Call the linker first
+link_code_settings
+
+# Move settings.json file
+rm ~/.config/Code/User/settings.json
+cp  --preserve=all ./code/settings.json ~/.config/Code/User/settings.json
 cp ./code/keybindings.json ~/.config/Code/User/keybindings.json
+echo -e "${Orange}Copying of vs code files has been finished!"
