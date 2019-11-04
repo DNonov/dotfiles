ce47f175081703e75dd41c2a960268672b55c7e2
Dimitar Nonov <d.nonov@gmail.com>
2019-11-04 22:25:01 +0000

    [fix] Change file path in get_extensions script

--- code/get_extensions.sh
+++ code/get_extensions.sh
@@ -4,13 +4,13 @@ set -e;
 
 # Get all installed extensions
 function get_extensions() {
-  touch extensions_list.txt;
+  touch ./code/extensions_list.txt;
   code --list-extensions >> extensions_list.txt;
 }
 
 # Does extensions_list.txt exist
-if [ -e "extensions_list.txt" ]; then
-  rm extensions_list.txt;
+if [ -e "./code/extensions_list.txt" ]; then
+  rm ./code/extensions_list.txt;
   get_extensions
 else
   get_extensions
