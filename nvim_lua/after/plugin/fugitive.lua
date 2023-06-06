67a3955daed55e21388e7d88f3244d23c8a34e9f
Dimitar Nonov <d.nonov@gmail.com>
2023-06-06 07:51:46 +0300

    [feat] Latest from Macbook

--- nvim_lua/after/plugin/fugitive.lua
+++ nvim_lua/after/plugin/fugitive.lua
@@ -1,1 +1,10 @@
-vim.keymap.set("n", "<leader>gs", vim.cmd.Git);
+-- Git operations around stash
+vim.keymap.set("n", "<leader>.", vim.cmd.Git);
+vim.keymap.set("n", "<leader>gsl", ":Git log -g stash <CR>");
+vim.keymap.set("n", "<leader>gs", ":Git stash save");
+vim.keymap.set("n", "<leader>gsp", ":Git stash pop <CR>");
+
+-- Git operations around branch
+vim.keymap.set("n", "<leader>gb", ":Git checkout ");
+
+
